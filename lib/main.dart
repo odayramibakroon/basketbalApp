10ceb85283fa71ae2b3b755683119984accf44f4
oday rami <bak.odey@gmail.com>
2025-10-20 00:13:34 +0300

    b

--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,15 @@ class MyApp extends StatelessWidget {
 
    @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Basketbalhome(),
+      home:   Basketbalhome(),
     );
   }
 }
